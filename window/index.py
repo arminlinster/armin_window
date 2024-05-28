3e6eb384443a401c71ff06bdc092c447d9fb8fed
arminlinster <arminlinster@gmail.com>
2024-05-28 11:25:55 +0800

    m0528 1125

--- window/index.py
+++ window/index.py
@@ -1,12 +1,19 @@
+'''
 with open('names.txt', encoding='utf-8') as file1:
     content:str = file1.read()
-    '''
-    print("----")
-    print(file1.read())
-    #print(content)
-    '''
-    names:list[str] = content.split()
+    
+    names:list[str]  = content.split()
     len(names)
     print(len(names))
     pass
 print(file1.closed)
+'''
+def get_names() -> list[str]:
+    with open('names.txt', encoding='utf-8') as file1:
+        content:str = file1.read()
+    names:list[str]  = content.split()
+    return names    
+
+names:list[str] = get_names()
+print(names)
+print(len(names))
