2d93c00fb30dddd986ffaa99ffa4a99cb726aa86
arminlinster <arminlinster@gmail.com>
2024-05-28 15:08:44 +0800

    m0528 1508

--- window_layout/index.py
+++ window_layout/index.py
@@ -1,10 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+names2 = "這是第一支GUI程式的內容"
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.title("pack1")
+        
+        btn1:ttk.Button = ttk.Button(self,text="Top")
+        btn1.pack()
+        btn2:ttk.Button = ttk.Button(self,text="Middle")
+        btn2.pack()
+        btn3:ttk.Button = ttk.Button(self,text="Bottom")
+        btn3.pack()
+        label:ttk.LabelFrame = ttk.Label(self,
+                                         text=names2,
+                                         font=("arial",20,"bold"),
+                                         foreground ='#f00',
+                                         background = '#0ff')
+        label.pack(padx=100, pady=50)
 
 if __name__ == '__main__':
     window:Window = Window()
